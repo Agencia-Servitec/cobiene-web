95c0ed3f48d2b2138539d30372c9c84bcc0fa963
EduardXI <eduardo.salazar@agenciaservitec.com>
2022-06-10 18:19:23 -0500

    ultimas img transformadas a webp carpeta.

--- src/data-list/departments.js
+++ src/data-list/departments.js
@@ -6,55 +6,55 @@ export const departments = [
         title:"jace",
         titleLarge: "Jefatura de Administración de Casas del Ejercito",
         banner: BannerJace,
-        url: "https://jace.com"
+        url: "https://jace.cobiene.mil.pe"
     },
     {
         title:"jas",
         titleLarge: "Jefatura de Apoyo Social",
         banner: BannerJas,
-        url: "https://jas.com"
+        url: "https://jas.cobiene.mil.pe"
     },
     {
         title:"jae",
         banner: BannerJae,
         titleLarge: "Jefatura de Apoyo Educativo",
-        url: "https://jae.com"
+        url: "https://jae.cobiene.mil.pe"
     },
     {
         title:"fossep",
         banner: BannerFossep,
         titleLarge: "Fondo Solidario de Sepelio del Ejercito del Perú",
-        url: "https://fossep.com"
+        url: "https://fossep.cobiene.mil.pe"
     },
     {
         title:"fovime",
         banner: BannerFovime,
         titleLarge: "Fondo de Vivienda Militar del Ejercito",
-        url: "https://fovime.com"
+        url: "https://fovime.cobiene.mil.pe"
     },
     {
         title:"acmp",
         banner: BannerAcmp,
         titleLarge: "Asociación Círculo Militar del Perú",
-        url: "https://acmp.com"
+        url: "https://acmp.cobiene.mil.pe"
     },
     {
         title:"bce",
         banner: BannerBce,
         titleLarge: "Bazar Central del Ejercito",
-        url: "https://bce.com"
+        url: "https://bce.cobiene.mil.pe"
     },
     {
         title:"cmsts",
         banner: BannerCmsts,
         titleLarge: "Círculo Militar Supervisores Tecnicos de Sub Oficiales",
-        url: "https://cmsts.com"
+        url: "https://cmsts.cobiene.mil.pe"
     },
     {
         title:"tramites",
         banner: BannerTramites,
         titleLarge: "Solicitudes y Tramites",
-        url: "https://tramites.com"
+        url: "https://tramites.cobiene.mil.pe"
     }
 
 ]
