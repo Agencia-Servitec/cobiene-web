3757f0f35558130fb2651c88c94bb5032ed31472
EduardXI <eduardo.salazar@agenciaservitec.com>
2022-06-13 19:51:40 -0500

    add news dates

--- src/data-list/departments.js
+++ src/data-list/departments.js
@@ -3,54 +3,64 @@ import {BannerJace,BannerJas,BannerJae,BannerFossep,BannerFovime,BannerAcmp,Bann
 export const departments = [
 
     {
+        id:"jace-01",
         title:"jace",
         titleLarge: "Jefatura de Administración de Casas del Ejercito",
         banner: BannerJace,
-        url: "https://jace.cobiene.mil.pe"
+        url: "https://www.gob.pe/15287-solicitar-asignacion-de-casa-de-servicio-temporal-a-la-jefatura-de-administracion-de-casas-del-ejercito-jace",
     },
     {
+        id:"jas-02",
         title:"jas",
         titleLarge: "Jefatura de Apoyo Social",
         banner: BannerJas,
-        url: "https://jas.cobiene.mil.pe"
+        url: "",
+        withSubDepartments: true
     },
     {
+        id:"jae-03",
         title:"jae",
         banner: BannerJae,
         titleLarge: "Jefatura de Apoyo Educativo",
-        url: "https://jae.cobiene.mil.pe"
+        url: "http://jeduce.ejercito.mil.pe/"
     },
     {
+        id:"fossep-04",
         title:"fossep",
         banner: BannerFossep,
         titleLarge: "Fondo Solidario de Sepelio del Ejercito del Perú",
-        url: "https://fossep.cobiene.mil.pe"
+        url: "https://www.fossep.com.pe/"
     },
     {
+        id:"fovime-05",
         title:"fovime",
         banner: BannerFovime,
         titleLarge: "Fondo de Vivienda Militar del Ejercito",
-        url: "https://fovime.cobiene.mil.pe"
+        url: "https://fovime.com/"
     },
     {
+        id:"acmp-06",
         title:"acmp",
         banner: BannerAcmp,
         titleLarge: "Asociación Círculo Militar del Perú",
-        url: "https://acmp.cobiene.mil.pe"
+        url: "https://www.circulomilitardelperu.com/"
     },
     {
+        id:"bce-07",
         title:"bce",
         banner: BannerBce,
         titleLarge: "Bazar Central del Ejercito",
-        url: "https://bce.cobiene.mil.pe"
+        url: "www.tubazar.com.pe"
     },
     {
+        id:"cmsts-08",
         title:"cmsts",
         banner: BannerCmsts,
         titleLarge: "Círculo Militar Supervisores Tecnicos de Sub Oficiales",
         url: "https://cmsts.cobiene.mil.pe"
     },
     {
+        id:"tramites-09",
         title:"tramites",
         banner: BannerTramites,
         titleLarge: "Solicitudes y Tramites",
---
+++ src/data-list/sub-departments.js
@@ -0,0 +1,34 @@
+import {BannerJace} from "../images";
+
+
+export const subDepartments = [
+
+    {
+        id:"sad-01",
+        departmentId: "jas-02",
+        title:"sad",
+        titleLarge: "Sección de Apoyo al Discapacitado",
+        banner: BannerJace,
+        url: "https://www.gob.pe/15287-solicitar-asignacion-de-casa-de-servicio-temporal-a-la-jefatura-de-administracion-de-casas-del-ejercito-jace"
+     },
+
+    {
+        id:"saed-02",
+        departmentId: "jas-02",
+        title:"saed",
+        titleLarge: "Sección de Apoyo Educativo",
+        banner: BannerJace,
+        url: "https://www.gob.pe/15287-solicitar-asignacion-de-casa-de-servicio-temporal-a-la-jefatura-de-administracion-de-casas-del-ejercito-jace"
+    },
+
+    {
+        id:"saeco-03",
+        departmentId: "jas-02",
+        title:"saeco",
+        titleLarge: "Sección de Apoyo Economico",
+        banner: BannerJace,
+        url: "https://www.gob.pe/15287-solicitar-asignacion-de-casa-de-servicio-temporal-a-la-jefatura-de-administracion-de-casas-del-ejercito-jace"
+    },
+
+
+]
