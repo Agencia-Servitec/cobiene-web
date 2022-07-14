d68409dc6a425b081a71669dce139306d564e5c3
EduardXI <eduardo.salazar@agenciaservitec.com>
2022-07-14 12:51:46 -0500

    change title of jas for das

--- src/data-list/departments.js
+++ src/data-list/departments.js
@@ -1,72 +1,77 @@
-import {BannerJace,BannerJas,BannerJae,BannerFossep,BannerFovime,BannerAcmp,BannerBce,BannerCmsts,BannerTramites} from  "../images";
+import {
+  BannerJace,
+  BannerJas,
+  BannerJae,
+  BannerFossep,
+  BannerFovime,
+  BannerAcmp,
+  BannerBce,
+  BannerCmsts,
+  BannerTramites,
+} from "../images";
 
 export const departments = [
-
-    {
-        id:"jace-01",
-        title:"jace",
-        titleLarge: "Jefatura de Administración de Casas del Ejercito",
-        banner: BannerJace,
-        url:"http://jace.cobiene.mil.pe/",
-
-    },
-    {
-        id:"jae-03",
-        title:"jae",
-        banner: BannerJae,
-        titleLarge: "Jefatura de Apoyo Educativo",
-        url:"https://jae.cobiene.mil.pe/"
-
-    },
-    {
-        id:"dae-02",
-        title:"dae",
-        titleLarge: "Jefatura de Apoyo Social",
-        banner: BannerJas,
-        withSubDepartments: true
-
-    },
-    {
-        id:"fossep-04",
-        title:"fossep",
-        banner: BannerFossep,
-        titleLarge: "Fondo Solidario de Sepelio del Ejercito del Perú",
-        url: "https://www.fossep.com.pe/"
-    },
-    {
-        id:"fovime-05",
-        title:"fovime",
-        banner: BannerFovime,
-        titleLarge: "Fondo de Vivienda Militar del Ejercito",
-        url: "https://fovime.com/"
-    },
-    {
-        id:"acmp-06",
-        title:"acmp",
-        banner: BannerAcmp,
-        titleLarge: "Asociación Círculo Militar del Perú",
-        url: "https://www.circulomilitardelperu.com/"
-    },
-    {
-        id:"bce-07",
-        title:"bce",
-        banner: BannerBce,
-        titleLarge: "Bazar Central del Ejercito",
-        url: "http://www.tubazar.com.pe/"
-    },
-    {
-        id:"cmsts-08",
-        title:"cmsts",
-        banner: BannerCmsts,
-        titleLarge: "Círculo Militar Supervisores Tecnicos de Sub Oficiales",
-        url: "https://cmsts.cobiene.mil.pe"
-    },
-    {
-        id:"tramites-09",
-        title:"tramites",
-        banner: BannerTramites,
-        titleLarge: "Solicitudes y Tramites",
-        url: "https://tramites.cobiene.mil.pe"
-    }
-
-]
+  {
+    id: "jace-01",
+    title: "jace",
+    titleLarge: "Jefatura de Administración de Casas del Ejercito",
+    banner: BannerJace,
+    url: "http://jace.cobiene.mil.pe/",
+  },
+  {
+    id: "jae-03",
+    title: "jae",
+    banner: BannerJae,
+    titleLarge: "Jefatura de Apoyo Educativo",
+    url: "https://jae.cobiene.mil.pe/",
+  },
+  {
+    id: "dae-02",
+    title: "das",
+    titleLarge: "Departamento de Apoyo Social",
+    banner: BannerJas,
+    withSubDepartments: true,
+  },
+  {
+    id: "fossep-04",
+    title: "fossep",
+    banner: BannerFossep,
+    titleLarge: "Fondo Solidario de Sepelio del Ejercito del Perú",
+    url: "https://www.fossep.com.pe/",
+  },
+  {
+    id: "fovime-05",
+    title: "fovime",
+    banner: BannerFovime,
+    titleLarge: "Fondo de Vivienda Militar del Ejercito",
+    url: "https://fovime.com/",
+  },
+  {
+    id: "acmp-06",
+    title: "acmp",
+    banner: BannerAcmp,
+    titleLarge: "Asociación Círculo Militar del Perú",
+    url: "https://www.circulomilitardelperu.com/",
+  },
+  {
+    id: "bce-07",
+    title: "bce",
+    banner: BannerBce,
+    titleLarge: "Bazar Central del Ejercito",
+    url: "http://www.tubazar.com.pe/",
+  },
+  {
+    id: "cmsts-08",
+    title: "cmsts",
+    banner: BannerCmsts,
+    titleLarge: "Círculo Militar Supervisores Tecnicos de Sub Oficiales",
+    url: "https://cmsts.cobiene.mil.pe",
+  },
+  {
+    id: "tramites-09",
+    title: "tramites",
+    banner: BannerTramites,
+    titleLarge: "Solicitudes y Tramites",
+    url: "https://cobiene.mil.pe/",
+  },
+];
